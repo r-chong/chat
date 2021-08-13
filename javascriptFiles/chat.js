df1ef5f8a0957cefb91579a7a1b3b93b7b1eaab2
r-chong <r-chong@github.com>
2021-08-13 18:19:16 -0400

    Edited chat flush so it doesnt look awful Plus fixed the emoji bar AND deleted trash files

--- javascriptFiles/chat.js
+++ javascriptFiles/chat.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 ////////////////////
 /*Chat >> VARIABLES*/
 ////////////////////
-let uiChatTextbox = document.getElementById('chatTextbox');
-let uiChatSubmit = document.getElementById('chatSubmit');
-let chatLogContainer = document.getElementById('log_cont');
-let chatControlsContainer = document.getElementById('chatControlsContainer');
-let uiHideDetails = document.getElementById('detailsToggle');
-let uiChatLog = document.getElementById('chatLog');
+let uiChatTextbox = document.getElementById("chatTextbox");
+let uiChatSubmit = document.getElementById("chatSubmit");
+let chatLogContainer = document.getElementById("log_cont");
+let chatControlsContainer = document.getElementById("chatControlsContainer");
+let uiHideDetails = document.getElementById("detailsToggle");
+let uiChatLog = document.getElementById("chatLog");
 let map = { Control: false, b: false, i: false, z: false };
 
 function textboxKeyup(event) {
-  if (event.key === 'Enter') {
+  if (event.key === "Enter") {
     event.preventDefault();
     sendString();
     uiChatSubmit.click();
@@ -24,12 +24,12 @@ function textboxKeyup(event) {
 function textboxKeydown(event) {
   if (event.key in map) {
     map[event.key] = true;
-    if (map['Control'] && map['b']) {
-      uiChatTextbox.innerText += '<b></b>';
+    if (map["Control"] && map["b"]) {
+      uiChatTextbox.innerText += "<b></b>";
       let valueOfTextbox = uiChatTextbox.innerText.length;
       uiChatTextbox.setSelectionRange(valueOfTextbox - 4, valueOfTextbox - 4);
-    } else if (map['Control'] && map['i']) {
-      uiChatTextbox.innerText += '<i></i>';
+    } else if (map["Control"] && map["i"]) {
+      uiChatTextbox.innerText += "<i></i>";
       let valueOfTextbox = uiChatTextbox.innerText.length;
       uiChatTextbox.setSelectionRange(valueOfTextbox - 4, valueOfTextbox - 4);
     }
@@ -45,7 +45,7 @@ function replaceHashtags(impureString) {
   let cleanString = impureString.replace(
     /(\#\w+)/g,
     function (unformattedText) {
-      return '<chat-hashtag>' + unformattedText + '</chat-hashtag>';
+      return "<chat-hashtag>" + unformattedText + "</chat-hashtag>";
     }
   );
   return cleanString;
@@ -55,7 +55,7 @@ function replaceLinks(impureString) {
   let cleanString = impureString.replace(
     /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=])*)/gi,
     function (url) {
-      return '<a' + " target='_blank'" + " href='" + url + "'>" + url + '</a>';
+      return "<a" + " target='_blank'" + " href='" + url + "'>" + url + "</a>";
     }
   );
   return cleanString;
@@ -63,14 +63,14 @@ function replaceLinks(impureString) {
 
 //Formats chat messages to look like SMS but Geniusised
 function chatMessageFormatter() {
-  let chatMsgs = document.querySelectorAll('.chatline');
+  let chatMsgs = document.querySelectorAll(".chatline");
   for (let i = 0; i < chatMsgs.length; i++) {
-    let username = getCookie('user');
-    let msg = chatMsgs[i]['outerHTML'];
+    let username = getCookie("user");
+    let msg = chatMsgs[i]["outerHTML"];
     if (
-      msg.includes('<span class="chat-message__content">[' + username + ']:')
+      msg.includes('<span class="chat-message__content">[' + username + "]:")
     ) {
-      chatMsgs[i].classList.add('chat-msg--me');
+      chatMsgs[i].classList.add("chat-msg--me");
     }
     //use else if to add specific colors based on other usernames make it BETTER
   }
@@ -78,14 +78,16 @@ function chatMessageFormatter() {
 
 //Show or hide details
 function detailsToggle() {
-  var element = document.getElementsByClassName('details');
-  if (uiHideDetails.checked == false) { //if details hidden, show details  
+  var element = document.getElementsByClassName("details");
+  if (uiHideDetails.checked == false) {
+    //if details hidden, show details
     for (let i = 0; i < element.length; i++) {
-      element[i].classList.remove('hidden');
+      element[i].classList.remove("hidden");
     }
-  } else if (uiHideDetails.checked == true) { //if details shown, hide details
+  } else if (uiHideDetails.checked == true) {
+    //if details shown, hide details
     for (let i = 0; i < element.length; i++) {
-      element[i].classList.add('hidden');
+      element[i].classList.add("hidden");
     }
   }
 }
@@ -94,33 +96,33 @@ function sendString() {
   var dirty = uiChatTextbox.innerText;
   var clean = DOMPurify.sanitize(dirty, {
     ALLOWED_TAGS: [
-      'b',
-      'em',
-      'mark',
-      'del',
-      'a',
-      'ins',
-      'code',
-      'sup',
-      'sub',
-      'progress',
-      'ol',
-      'ul',
-      'li',
-      'h1',
-      'h2',
-      'h3',
+      "b",
+      "em",
+      "mark",
+      "del",
+      "a",
+      "ins",
+      "code",
+      "sup",
+      "sub",
+      "progress",
+      "ol",
+      "ul",
+      "li",
+      "h1",
+      "h2",
+      "h3",
     ],
-    FORBID_ATTR: ['style', 'class', 'id', 'href', 'src', 'onclick'],
+    FORBID_ATTR: ["style", "class", "id", "href", "src", "onclick"],
   }); //clean that yucky stuff
-  clean = clean.replace(/\_\_(.*)\_\_/gim, '<em>$1</em>');
-  clean = clean.replace(/\-\-(.*)\-\-/gim, '<del>$1</del>');
-  clean = clean.replace(/\*\*(.*)\*\*/gim, '<b>$1</b>');
-  clean = clean.replace(/\!\!(.*)\!\!/gim, '<h1>$1</h1>');
-  clean = clean.replace(/\+/g, '%2b');
+  clean = clean.replace(/\_\_(.*)\_\_/gim, "<em>$1</em>");
+  clean = clean.replace(/\-\-(.*)\-\-/gim, "<del>$1</del>");
+  clean = clean.replace(/\*\*(.*)\*\*/gim, "<b>$1</b>");
+  clean = clean.replace(/\!\!(.*)\!\!/gim, "<h1>$1</h1>");
+  clean = clean.replace(/\+/g, "%2b");
   clean = clean.replace(/\"/g, '\\"');
-  clean = clean.replace(/\&/g, '%26');
-  clean = clean.replace(/\:-/g, '−');
+  clean = clean.replace(/\&/g, "%26");
+  clean = clean.replace(/\:-/g, "−");
   //Replaces the reply placeholder
   clean = clean.replace(/\↪️/g, msgTxtContent);
   //Custom Emojis (stickers)
@@ -218,16 +220,16 @@ function sendString() {
   clean = replaceHashtags(clean);
   var savestring = clean;
   //IMPROVEMENT: Why not just use var clean? Wat? Why make another variable?
-  if (savestring != '') {
-    var username = getCookie('user');
+  if (savestring != "") {
+    var username = getCookie("user");
     // the updated save string [user] + savestring
 
-    savestring = '[' + username + ']: ' + savestring;
+    savestring = "[" + username + "]: " + savestring;
 
     // save the chat string
     saveToDB(savestring, getChatFromDB);
     // set the textbox to empty
-    uiChatTextbox.innerText = '';
+    uiChatTextbox.innerText = "";
   }
   chatMessageFormatter();
   scrollBottom();
@@ -243,10 +245,10 @@ function saveToDB(savestring, onSuccess) {
       onSuccess();
     }
   };
-  var apikey = getCookie('logged_in');
-  var poststring = 'action=save&savestring=' + savestring + '&apikey=' + apikey;
-  xhttp.open('POST', 'chat.php', true);
-  xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded'); // php seems to need this
+  var apikey = getCookie("logged_in");
+  var poststring = "action=save&savestring=" + savestring + "&apikey=" + apikey;
+  xhttp.open("POST", "chat.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); // php seems to need this
   xhttp.send(poststring);
 }
 
@@ -257,19 +259,19 @@ function getChatFromDB() {
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       var responses = JSON.parse(this.responseText);
-      var formatted_chatlog = '';
+      var formatted_chatlog = "";
       // let username = getCookie('user');
       for (let i = 0; i < responses.length; i++) {
         formatted_chatlog =
           formatted_chatlog +
           '<div class="chatline">' +
           '<span class="details">' +
-          responses[i]['date'] + 
+          responses[i]["date"] +
           // '[' + username + ']' Add username to chat details, DO NOT REMOVE -Reese
-          '</span>' + 
+          "</span>" +
           '<span class="chat-message__content">' +
-          responses[i]['chatstring'] + //message content
-          '</span></div > ';
+          responses[i]["chatstring"] + //message content
+          "</span></div > ";
       }
       uiChatLog.innerHTML = formatted_chatlog;
       scrollBottom();
@@ -277,27 +279,27 @@ function getChatFromDB() {
       chatMessageFormatter();
     }
   };
-  var poststring = 'action=getchat&apikey=' + getCookie('logged_in');
-  xhttp.open('POST', 'chat.php', true);
-  xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded'); // php seems to need this
+  var poststring = "action=getchat&apikey=" + getCookie("logged_in");
+  xhttp.open("POST", "chat.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); // php seems to need this
   xhttp.send(poststring);
 }
 
 function executeChatFuncs() {
-  uiChatSubmit.addEventListener('click', function () {
+  uiChatSubmit.addEventListener("click", function () {
     sendString();
     getChatFromDB();
     uiChatTextbox.focus();
   });
-  uiChatTextbox.addEventListener('keydown', textboxKeydown);
-  uiChatTextbox.addEventListener('keyup', textboxKeyup);
-  uiHideDetails.addEventListener('click', detailsToggle);
+  uiChatTextbox.addEventListener("keydown", textboxKeydown);
+  uiChatTextbox.addEventListener("keyup", textboxKeyup);
+  uiHideDetails.addEventListener("click", detailsToggle);
 }
 
-document.addEventListener('readystatechange', (event) => {
-  if (event.target.readyState === 'loaded') {
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "loaded") {
     executeChatFuncs();
-  } else if (event.target.readyState === 'complete') {
+  } else if (event.target.readyState === "complete") {
     executeChatFuncs();
   }
 });
